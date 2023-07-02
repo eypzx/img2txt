a266da6ce7a1218b39bb161bc269fd7695ed74e2
Eric Y <51849178+p0xt@users.noreply.github.com>
2023-07-01 20:30:58 -0700

    initial commit

---
+++ img2txt.py
@@ -0,0 +1,55 @@
+# img2txt takes an image file as input and extracts the text content from the image using Tesseract-OCR, displaying the extracted text on the terminal.
+# Author: Eric Yepiz
+
+
+import sys
+import pytesseract
+from PIL import Image
+
+
+
+# Path to the Tesseract-OCR executable
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
+def extract_text_from_image(image_path):
+    
+    try:
+        # open the image file
+        image = Image.open(image_path)
+
+        # convert the image to grayscale
+        grayscale_image = image.convert('L')
+
+        # use pytesseract to extract text from the image
+        text = pytesseract.image_to_string(grayscale_image)
+
+        return text
+    except FileNotFoundError:
+        print(f"Image file not found: {image_path}")
+    except Exception as e:
+        print("An error occurred:", str(e))
+        print(f"Path to image: {image_path}")
+
+
+
+def main():
+    
+    #Check if the image file path is provided as a command line argument
+    if len(sys.argv) < 2:
+        print("Usage: python3 img2txt.py <image_path>")
+        return
+
+
+    # get the image file from the command line argument
+    image_path = sys.argv[1]
+
+    # Extract text from the image
+    extracted_text = extract_text_from_image(image_path)
+
+    # print the extracted text
+    print(extracted_text)
+
+
+
+if __name__ == '__main__':
+    main()
